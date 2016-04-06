07fcdaf3eef6e5e15379737da289b206e94508de
Michael <michael@noltenet.de>
2016-04-06 09:08:33 +0000

    .

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
+//URL des Meshviewers
 var gMeshviewer = "http://mesh.ffnw.de/";
+
+//URL zur Logodatei
 var gLogo = "https://blog.nordwest.freifunk.net/wp-content/themes/freifunk-nordwest-wordpress-design/images/logo.png";
-//var gMeshviewer = "https://bremen.freifunk.net/meshviewer/"; 
-//var gMeshviewer = "http://mesh.ffnord.net/";
-//var gMeshviewer = "http://map.freifunk-moers.de/";
+
